a39fa1513cf618b834320c71c1bd5de996907f51
Gabriel Negrão Silva <itsg_negrao@hotmail.com>
2016-10-06 16:32:59 -0300

    Commit resolução rota login

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
 
   resources :socios
-  resources :socios
   get 'home/index'
-  root 'home#index'
-
 
   resources :login
   get    'login'   => 'login#new'
   post   'login'   => 'login#create'
   delete 'logout'  => 'login#destroy'
+  root 'login#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
