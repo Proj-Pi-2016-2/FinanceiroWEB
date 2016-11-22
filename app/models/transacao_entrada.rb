20d13ba15c11b8471fb8b45c6f4039138a62ca70
GIOVANI HENRIQUE BERTUZZO <giovani.hbertuzzo@gmail.com>
2016-11-22 20:38:05 -0200

    Validação dos campos Data, Valor e Justificativa de Entrada nas transações de entrada

--- app/models/transacao_entrada.rb
+++ app/models/transacao_entrada.rb
@@ -1,2 +1,8 @@
 class TransacaoEntrada < ActiveRecord::Base
+
+validates_presence_of :valor , message: 'não pode ficar em branco'
+validates_presence_of :data_entrada , message: 'não pode ficar em branco'
+validates_presence_of :justifica_entrada , message: 'não pode ficar em branco'
+validates_numericality_of :valor, greater_than: 0, message: 'tem que ser um número maior que 0'
+
 end
