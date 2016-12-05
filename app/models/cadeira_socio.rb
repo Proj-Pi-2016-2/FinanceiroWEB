d12c0e842d5370f1854d573d5726945cf62c65c1
Marcos Golom <viniciusgolom@gmail.com>
2016-12-05 16:41:22 -0200

    validacao de cdeira > 0

--- app/models/cadeira_socio.rb
+++ app/models/cadeira_socio.rb
@@ -2,4 +2,5 @@ class CadeiraSocio < ActiveRecord::Base
 	 has_and_belongs_to_many :socios, :class_name => "CadeiraSocio",
     :foreign_key => "cpf", :association_foreign_key => "cpf"
     has_many :cadeiras
+    validates_numericality_of :cadeira, greater_than: 0, message: 'tem que ser um número maior que 0'
 end
