39a74d398b5461377ddc1124436422bcdab3b3ff
Gabriel Negrão Silva <itsg_negrao@hotmail.com>
2016-12-05 17:42:26 -0200

    Routes reconfigurado

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require moment
-//= require bootstrap-datepicker
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
  def new
- 
+
  end
- 
+
  def create
  	user = Socio.find_by(email: params[:email])
  		if user && user.authenticate(params[:senha])
@@ -15,10 +15,22 @@ class SessionsController < ApplicationController
  		end
 
  end
- 
+
  def destroy
  	session[:id] = nil
  	redirect_to login_path, notice: "Deslogado com sucesso!"
+ end
+
+ def set_roots
+   if current_user
+     if session[:id] == 1
+       redirect_to socios_path
+     else
+       redirect_to "/mensalidades?utf8=✓&commit=Filtrar%21&search=" + session[:id].to_s
+     end
+   else
+     redirect_to "/login"
+   end
  end
 
 end
