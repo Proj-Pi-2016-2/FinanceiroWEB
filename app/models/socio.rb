825e338c10d8252e1997ef9f8a71c5617282ee1b
Giovani <giovani.hbertuzzo@gmail.com>
2016-12-06 09:34:46 -0200

    Validacoes, concerto de bugs transacoes de entrada/socios

--- app/models/socio.rb
+++ app/models/socio.rb
@@ -9,12 +9,16 @@ class Socio < ActiveRecord::Base
   validates_cpf_format_of :cpf, message: 'inválido inserido'
 
   validates_presence_of :nome , message: 'não pode ficar em branco'
+  validates :nome, length: {in: 5..60, message: 'deve ter entre 5 e 60 caracteres'}
 
   validates_presence_of :telefone , message: 'não pode ficar em branco'
 
   validates_presence_of :endereco , message: 'não pode ficar em branco'
+  validates :endereco, length: {in: 10..120, message: 'deve ter entre 10 e 120 caracteres'}
 
   validates_presence_of :cadeira , message: 'não pode ficar em branco'
+  validates_uniqueness_of :cadeira , message: 'já existe'
+  validates_numericality_of :cadeira, greater_than: 0, message: 'tem que ser positivo'
 
   validates_presence_of :sexo , message: 'não pode ficar em branco'
 
